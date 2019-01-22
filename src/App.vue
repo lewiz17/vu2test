18683d8511b66a063d6219837cd79ffd9b80c247
Lewiz Acosta <mp3edw@gmail.com>
2019-01-22 17:49:31 -0500

    Vue styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"> 
+    <Mamita/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Mamita from './components/skills.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Mamita
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    width: 50%;
 }
 </style>
---
+++ src/components/skills.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <input type="text" placeholder="Ingresa algun skill que poseas" v-model="skill">
+      {{skill}}
+      <ul>
+        <li v-for="(data,i) in Skills" :key="i">{{i}}.{{data.skill}}</li>
+      </ul>
+      <p>Arriba los skills que posees</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Mamita',
+  data() {
+    return {
+      skill: '',
+      Skills: [
+        {'skill': 'vuejs'},
+        {'skill': 'frontend'}
+      ]
+    }
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.holder{
+  background: #fff;
+}
+ul{
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+ul li{
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  border-left: 5px solid #3eb3f6;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+p{
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
+.container{
+  box-shadow: 0px 0px 40px lightgray;
+}
+input{
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 20px;
+  font-size: 1em;
+  background-color: #323333;
+  color: #687f7f;
+}
+</style>
